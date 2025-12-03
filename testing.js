a04577d1eff246833e282186dfa1a01d723c8666
mstanton6 <mstanton6@yahoo.com>
2025-12-02 20:20:40 -0500

    Initial commit for the 308H1 project

---
+++ testing.js
@@ -0,0 +1,57 @@
+// The initial numbers that must be verified.
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+// Check one: add up to 50
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+const isSum50 = (n1 + n2 + n3 + n4) == 50;
+console.log('isSum50 ' + isSum50);
+// Check two: at least two odd numbers
+// Here, we use modulus to check if something is odd.
+// Since % 2 is 0 if even and 1 if odd, we can use
+// arithmetic to count the total number of odd numbers.
+const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+
+// Check three: no number larger than 25
+// This time, we use the OR operator to check
+// if ANY of the numbers is larger than 25.
+const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+
+// Check four: all unique numbers
+// This is long, and there are more efficient
+// ways of handling it with other data structures
+// that we will review later.
+const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+
+// Here, we put the results into a single variable 
+// for convenience. Note how we negate isOver25 using
+// the ! operator. We could also have tested for 
+// "isUnder25" as an alternative.
+const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+
+// Finally, log the results.
+console.log(isValid);
+
+// Implement the following:
+
+// Check if all numbers are divisible by 5. Cache the result in a variable.
+const div5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0
+console.log('The 4 numbers are all divisible by 5: ' + div5);
+
+// Check if the first number is larger than the last. Cache the result in a variable.
+const div6 = n1 > n4;
+console.log('the first number is larger than the last: ' + div6);
+
+// Accomplish the following arithmetic chain.  1. Subtract the first number from the second number.
+// 2. Multiply the result by the third number. 3. Find the remainder of dividing the result by the fourth number.
+div7 = (n2 - n1) * n3
+
+div8 = div7 % n4
+console.log('arithmetic chain: ' + div8);
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+const allUnder25 = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
+console.log('Change the way that isOver25 calculates: ' + allUnder25);
